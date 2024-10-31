e6b065737a0750a0c10d52e3a3512d6ccf514b9a
unknown <enchan101@gmail.com>
2024-10-31 18:30:30 -0400

    filters in search bar

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -112,6 +112,11 @@ const Map: React.FC = () => {
 
 // Search form
 const SearchForm: React.FC<{ onClose: () => void }> = ({ onClose }) => {
+  const [showFilters, setShowFilters] = useState(false);
+
+  const handleFiltersToggle = () => {
+    setShowFilters(!showFilters);
+  };
   return (
     <div id="search-container" className="slide-up">
       <div id="search-form">
@@ -124,6 +129,19 @@ const SearchForm: React.FC<{ onClose: () => void }> = ({ onClose }) => {
           id="search-query"
           placeholder="Search for an address"
         />
+        <a id="filters-toggle-button" onClick={handleFiltersToggle}>
+          Filters
+        </a>
+        {showFilters && (
+          <div id="filters">
+            <div className="filter-group">
+              <input type="text" id="price-filter" placeholder="Price" />
+              <span className="filters-label">CAD</span>
+              <input type="text" id="distance-filter" placeholder="Distance" />
+              <span className="filters-label">km</span>
+            </div>
+          </div>
+        )}
         <button id="search-button">Search</button>
       </div>
     </div>
