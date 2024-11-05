4b09ee1acbeed57c97a05448d175dc9868f83127
Jerry Chen <114103837+jchen03@users.noreply.github.com>
2024-11-04 20:34:44 -0500

    Delete backend directory

--- backend/app/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class React(models.Model):
-    email = models.EmailField()
-    password = models.CharField(max_length=100)
-
-    # def __str__(self):
-    #     return self.name
