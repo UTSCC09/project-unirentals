a3f966be68880c47ebd355b3ad9e90cdab5b8c8d
unknown <kieran.r.hansen@gmail.com>
2024-11-05 22:41:20 -0500

    Finished backend setup on local machine, also fixed some minor errors with the file formatting

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -42,6 +42,9 @@ INSTALLED_APPS = [
     'app'
 ]
 
+# CORS_ORIGIN_ALLOW_ALL = True
+# CORS_ALLOWED_ORIGINS = []
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -14,13 +14,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.contrib import admin
 from django.urls import path, include
-from rest_framework import routers
-from app.views import UserViewSet
-
-router = routers.DefaultRouter()
-router.register(r'users', UserViewSet)
 
 urlpatterns = [
-    path('', include(router.urls)),
+  path('admin/', admin.site.urls),
+  path('', include('app.urls')),
 ]
