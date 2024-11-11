5a8023bade7e844af803985860452dc1256124f3
KieranHansen <kieran.r.hansen@gmail.com>
2024-11-11 11:39:59 -0500

    Added back gitignore

--- backend/users/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-  path('api/getcsrf/', views.get_csrf, name="get_csrf"),
-  path('api/register/', views.registerUser, name="register"),
-  path('api/login/', views.loginUser, name="login"),
-  path('api/logout/', views.logoutUser, name="logout"),
-]
