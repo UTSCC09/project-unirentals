b0a17afa6c157ea1e1442422fb01bb21febaf2fa
unknown <kieran.r.hansen@gmail.com>
2024-11-11 01:00:15 -0500

    Added field types to profile model

--- backend/profiles/models.py
+++ backend/profiles/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from users.models import CustomUser
 
 # Create your models here.
+class userProfile(models.Model):
+  
+  user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
+
+  first_name = models.CharField()
+  last_name = models.CharField()
+
+  age = models.PositiveIntegerField()
+  pronouns = models.CharField()
+  school = models.CharField()
+
+  bio = models.TextField()
+
+  smokes = models.BooleanField()
+  pets = models.BooleanField()
+  drinks = models.BooleanField()
+
+  profilePic = models.ImageField()
