0c9ee292df60a8f6f18fb2953d39b78e063ed455
unknown <kieran.r.hansen@gmail.com>
2024-11-17 20:30:08 -0500

    Created get endpoint for all listings

--- backend/listings/views.py
+++ backend/listings/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from . import forms
+from users.models import CustomUser
+from .models import Listing
+from .serializers import ListingSerializer
 
 # Create your views here.
+@csrf_exempt
+def listingGenericView(request): #/api/listings/
+  if request.method == 'GET':
+    listings = Listing.objects.all()
+    serializer = ListingSerializer(listings, many=True)
+
+    return JsonResponse(serializer.data, status=200)
+  
+  return JsonResponse({"errors": "Method not allowed."}, status=405)
+
+
