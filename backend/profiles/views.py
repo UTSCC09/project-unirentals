73608f81f05140beca79020ab39153ccd0181283
unknown <kieran.r.hansen@gmail.com>
2024-11-18 00:12:14 -0500

    Some quick fixes to profile things, added validation check to POST requests, fixed url paths, and added new endpoints specifically for self information

--- backend/profiles/views.py
+++ backend/profiles/views.py
@@ -1,3 +1,4 @@
+from profile import Profile
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from . import forms
@@ -6,6 +7,51 @@ from .models import userProfile
 from .serializers import ProfileSerializer
 
 # Create your views here.
+@csrf_exempt
+def selfProfileInformation(request):
+  try:
+    profile = userProfile.objects.get(user=request.user)
+
+    if request.user.is_authenticated:
+      
+      if request.method == 'GET':
+        user_info = ProfileSerializer(profile)
+        return JsonResponse(user_info.data, status=200)
+      
+      if request.method == 'POST':
+        form = forms.profileForm(request.POST, request.FILES, instance=profile)
+          
+        if form.is_valid():
+          form.save()
+          return JsonResponse({"message": "Profile updated."}, status=200)
+        
+        return JsonResponse({"errors": form.errors}, status=400)
+      
+      return JsonResponse({"errors": "Method not allowed."}, status=405)
+
+    return JsonResponse({"errors": "User must be signed in to perform this action."}, status=401)
+
+  except userProfile.DoesNotExist:
+    return JsonResponse({"errors": "Profile does not exist."}, status=404)
+
+
+@csrf_exempt
+def selfProfilePicture(request):
+  try:
+    profile = userProfile.objects.get(user=request.user)
+
+    if request.method == 'GET':
+      
+      if profile.profile_pic:
+        return JsonResponse({"url": profile.profile_pic.url}, status=200)
+      
+      return JsonResponse({"errors": "User has no profile picture."}, status=405)
+    
+    return JsonResponse({"errors": "Method not allowed."}, status=405)
+    
+  except CustomUser.DoesNotExist:
+    return JsonResponse({"errors": "Profile does not exist."}, status=404)
+
 @csrf_exempt
 def userProfileInformation(request, id):
   try:
@@ -45,8 +91,10 @@ def userProfilePicture(request, id):
     profile = userProfile.objects.get(user=targetUser)
 
     if request.method == 'GET':
-      print(profile.profile_pic.url)
-      return JsonResponse({"url": profile.profile_pic.url}, status=200)
+      if profile.profile_pic:
+        return JsonResponse({"url": profile.profile_pic.url}, status=200)
+      
+      return JsonResponse({"errors": "User has no profile picture."}, status=405)
     
     return JsonResponse({"errors": "Method not allowed."}, status=405)
     
