e42a60a41e7d242fda7c79d7fdf06a8afcba64a6
unknown <kieran.r.hansen@gmail.com>
2024-11-17 20:29:10 -0500

    Fixed naming error in forms.py

--- backend/listings/forms.py
+++ backend/listings/forms.py
@@ -2,7 +2,7 @@ from django.forms import ModelForm
 from django import forms
 from .models import Listing
 
-class profileForm(ModelForm):
+class listingForm(ModelForm):
   class Meta:
     model = Listing
     fields = '__all__'
