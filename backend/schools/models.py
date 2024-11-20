0642363b9a0081d0e76bc42e37cc60fec990e6e2
unknown <kieran.r.hansen@gmail.com>
2024-11-19 21:04:30 -0500

    Fixed settings file, created school model

--- backend/schools/models.py
+++ backend/schools/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class School(models.Model):
+  name = models.CharField(max_length=100)
+
+  address = models.CharField(max_length=300)
+  longitude = models.FloatField()
+  latitude = models.FloatField()
