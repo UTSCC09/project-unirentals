d1613c407385a439ae1a140f757d8055e3b00df1
unknown <kieran.r.hansen@gmail.com>
2024-11-20 22:20:32 -0500

    Created view for getting all schools

--- backend/schools/views.py
+++ backend/schools/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from .models import School
 
 # Create your views here.
+def schoolView(request):
+  if request.method == 'GET':
+    schools = School.objects.all()
+    serializer = SchoolSerializer(schools, many=True)
+
+    return JsonResponse({"schools": serializer.data}, status=200)
+  
+  return JsonResponse({"errors": "Method not allowed."}, status=405)
