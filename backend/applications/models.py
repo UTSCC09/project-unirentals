5e4ab1108821a5f865ac95c37f7a2095acf14be6
unknown <kieran.r.hansen@gmail.com>
2024-11-25 22:47:33 -0500

    Created model for rental applications

--- backend/applications/models.py
+++ backend/applications/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from users.models import CustomUser
+from listings.models import Listing
 
 # Create your models here.
+class RentalApplication(models.Model):
+  
+  users = models.ManyToManyField(CustomUser, on_delete=models.CASCADE, related_name='user_applications')
+  listing = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name='listing_applications')
+  created_at = models.DateTimeField(auto_now_add=True, null=True)
+
+  def __str__(self):
+    return self.listing.address + " (id:" + self.id + ")"
