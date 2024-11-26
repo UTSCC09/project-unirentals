229c6debf0a83d0d23a6064a0e59089987ee5c4d
unknown <kieran.r.hansen@gmail.com>
2024-11-26 14:05:16 -0500

    Updated the model to automatically delete itself if no users are associated, and created migration files

--- backend/applications/models.py
+++ backend/applications/models.py
@@ -5,7 +5,7 @@ from listings.models import Listing
 # Create your models here.
 class RentalApplication(models.Model):
   
-  users = models.ManyToManyField(CustomUser, on_delete=models.CASCADE, related_name='user_applications')
+  users = models.ManyToManyField(CustomUser, related_name='user_applications')
   listing = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name='listing_applications')
   created_at = models.DateTimeField(auto_now_add=True, null=True)
   full = models.BooleanField(default=False)
@@ -14,8 +14,15 @@ class RentalApplication(models.Model):
     return self.listing.address + " (id:" + self.id + ")"
   
   def save(self, *args, **kwargs):
-    # Check if the number of applications matches the number of bedrooms, and set to full if so
-    if self.users.count() == self.listing.bedrooms:
-      self.full = True
-    
-    super().save(*args, **kwargs)
+    # Check if the application has associated users, if not, delete
+    if not self.users.exists():
+      super.delete()
+
+    # Save normally
+    else:
+      
+      # Check if the number of applications matches the number of bedrooms, and set to full if so
+      if self.users.count() == self.listing.bedrooms:
+        self.full = True
+
+      super().save(*args, **kwargs)
